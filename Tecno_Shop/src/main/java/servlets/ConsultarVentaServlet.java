4d8d9aacdd84ab1284be5e7b3b82943e64a2f7be
IRONMIGUE16 <miguelanga1604@gmail.com>
2022-10-01 23:37:46 -0500

    actualisacion Miguel

---
+++ Tecno_Shop/src/main/java/servlets/ConsultarVentaServlet.java
@@ -0,0 +1,81 @@
+package servlets;
+
+import beans.Venta;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import controller.VentaController;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet(name = "ConsultarVentaServlet", urlPatterns = {"/ConsultarVentaServlet"})
+public class ConsultarVentaServlet extends HttpServlet {
+
+    private final VentaController ventaController = new VentaController();
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * @throws java.sql.SQLException
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        String idStr = request.getParameter("id");
+        int id = Integer.parseInt(idStr);
+        Venta venta = ventaController.consultar(id);
+        Gson gson = new GsonBuilder().create();
+        String ventaJson = gson.toJson(usuario);
+        response.setContentType("application/json");
+        try ( PrintWriter out = response.getWriter()) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            out.write(ventaJson);
+            out.flush();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(ConsultarVentaServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
